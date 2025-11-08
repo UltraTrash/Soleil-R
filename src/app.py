a9c365ca50f8e1f702b4a28da1a4d9d0525baad3
UltraTrash <sjdjoos@gmail.com>
2025-11-07 22:08:48 -0600

    made a /src folder is good :thumbsup:

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 
-def test():
+def app():
     return render_template('index.html')
